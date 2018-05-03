e8be75af34248b43d531813088b5e21044623bdc
kylmcgr <kylmcgr@nuevaschool.org>
2018-05-03 09:53:16 -0700

    cartpole evolution

--- cartpoleEvolution.py
+++ cartpoleEvolution.py
@@ -1,37 +1,27 @@
 import gym
 from EvolutionaryNeuralNet import Evolution
-env = gym.make('MountainCar-v0')
+env = gym.make('CartPole-v1')
 # env = gym.make('FrozenLake-v0')
-evo = Evolution(2,[6],3)
+evo = Evolution(4,[6],2)
 def train():
-    best = -1
-    for i in range(200):
+    best = 0
+    for i in range(100):
         print (i)
         for genome in evo.genomes:
             observation = env.reset()
             score = 0
-            right = -1
-            for t in range(200):
+            for t in range(500):
                 actions = genome.network.compute(observation)
                 action = 0
                 if actions[0]>actions[1]:
-                    if actions[0]>actions[2]:
-                        action = 0
-                    else:
-                        action = 2
+                    action = 0
                 else:
                     action = 1
                 observation, reward, done, info = env.step(action)
                 score += reward
-                if observation[0]>right:
-                    right = observation[0]
                 if done:
-                    score = score + 300
                     break
-            if score == -200:
-                genome.score = right
-            else:
-                genome.score = score
+            genome.score = score
         evo.evolve()
         if evo.genomes[0].score > best:
             print (evo.genomes[0].score)
@@ -45,16 +35,12 @@ def show():
         actions = evo.genomes[0].network.compute(observation)
         action = 0
         if actions[0]>actions[1]:
-            if actions[0]>actions[2]:
-                action = 0
-            else:
-                action = 2
+            action = 0
         else:
             action = 1
         observation, reward, done, info = env.step(action)
         score += reward
         if done:
-            score = 1000-score
             break
     print (score)
 train()
